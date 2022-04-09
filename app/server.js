69cc443db7484343121fe47b2ae2e1e732ea3a4d
dang <trinhdangnhat@gmail.com>
2022-04-09 14:26:50 +0200

    set up docker mongo and mongoose

--- app/server.js
+++ app/server.js
@@ -1,47 +1,37 @@
-const { MongoClient } = require("mongodb");
-const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
+
+const { mongoConnect } = require("./services/mongo");
 const app = express();
-const router = express.Router();
 
 // use when starting application locally
 //let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+//let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
-async function connectData() {
-  let databaseName = "my-test";
-  const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-  const client = await MongoClient.connect(mongoUrlDocker, mongoClientOptions);
-
-  const db = client.db(databaseName);
+async function getData() {
+  const { db } = mongoose.connection;
   const items = await db.collection("users").find({}).toArray();
-  client.close();
-
-  console.log(items);
   return items;
 }
 
-/* app.get("/", (req, res) => {
-  res.send({ name: "hello" });
-}); */
-
-router.get("/", (req, res) => {
-  /*  res.send({ name: "Node mon test3" }); */
-  connectData().then((data) => {
+app.get("/", (req, res) => {
+  getData().then((data) => {
     res.send(data);
   });
 });
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-//add the router
-app.use("/", router);
+const PORT = process.env.PORT || 8000;
 
-const PORT = 8000;
+async function startServer() {
+  await mongoConnect();
+  app.listen(PORT, function () {
+    console.log(`server run in port ${PORT}`);
+  });
+}
 
-app.listen(PORT, function () {
-  console.log(`server run in port ${PORT}`);
-});
+startServer();
